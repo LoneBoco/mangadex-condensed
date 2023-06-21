c27096eb451a6b71143d2e02840eb9d10a244fde
John Norman <loneboco@gmail.com>
2023-06-21 08:30:05 -0700

    1.2: Updated to work again. Added rollover cover images.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MangaDex Condensed
 
 Condense the new MangaDex website and remove whitespace. As of now, this just affects the updated follows feed.
+
+![screenshot](./screenshots/screenshot_2.png)
--- mangadex-condensed.js
+++ mangadex-condensed.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MangaDex Condensed
 // @namespace    suckerfree
-// @version      1.1
+// @version      1.2
 // @description  Condense MangaDex for the whitespace impaired.
 // @author       Nalin, u/stonksonlydown
 // @match        https://mangadex.org/titles/feed
@@ -24,20 +24,56 @@
     head.appendChild(style);
   }
 
-  // Reduce the padding around the update group.
-  addGlobalStyle('div.v-application div.pa-4 {padding: 4px 8px !important;}');
+  // Remove bolding of the chapter titles.
+  addGlobalStyle('.chapter-list__title {font-weight: normal !important;}');
 
-  // Slightly reduce the font size of the chapter name.
-  addGlobalStyle('div.pa-4 h6.text-body-1 {font-size: 0.9rem !important;}');
+  // Slightly reduce the font size of the series name.
+  addGlobalStyle('.chapter-feed__title span {font-size: 0.9rem !important;}');
 
-  // Remove the manga image.
-  // TODO: Find a way to bring back on mouseover of the series name.
-  addGlobalStyle('div.pa-4 div.outer {display: none !important;}');
-
-  // Condense the whitespace around the separator line between the series name and the chapter updates.
-  addGlobalStyle('div.pa-4 div.mt-3 {margin: 4px !important; margin-top: 0px !important;}');
-
-  // Fix the column width of the chapter updates so the update time doesn't wrap to the next line.
-  addGlobalStyle('div.pa-4 .d-flex .col-lg-4 {flex: 0 0 30% !important; max-width: 30% !important;}');
-  addGlobalStyle('div.pa-4 .d-flex .pa-3 {padding: 0px !important;}');
 })();
+
+const apply_js_cb = function(mutationsList, observer) {
+  observer.disconnect();
+  console.log('Applying JS.');
+
+  let containers = document.getElementsByClassName('chapter-feed__container');
+  for (const container of containers) {
+    let title = container.getElementsByClassName('chapter-feed__title')[0];
+    let cover = container.getElementsByClassName('chapter-feed__cover')[0];
+    let chapters = container.getElementsByClassName('chapter-feed__chapters')[0];
+
+    if (title && cover && chapters) {
+      container.style.display = "block";
+      container.style.gap = "0.5rem";
+      title.style.paddingBottom = "0.5em";
+      cover.style.display = "none";
+      chapters.style.paddingTop = "0.5em";
+
+      title.addEventListener('mouseenter', e => {
+        container.style.display = "grid";
+        title.style.paddingBottom = "0";
+        cover.style.display = "block";
+        chapters.style.paddingTop = "0";
+      });
+      title.addEventListener('mouseleave', e => {
+        container.style.display = "block";
+        title.style.paddingBottom = "0.5em";
+        cover.style.display = "none";
+        chapters.style.paddingTop = "0.5em";
+      });
+    }
+  }
+};
+
+window.onload = (event) => {
+  const loading_cb = function(mutationsList, observer) {
+    observer.disconnect();
+
+    const page_container = document.getElementsByClassName('container mb-4 wide')[0].parentElement;
+    const chapter_observer = new MutationObserver(apply_js_cb);
+    chapter_observer.observe(page_container, {attributes: false, childList: true, subtree: true});
+  };
+
+  const load_observer = new MutationObserver(loading_cb);
+  load_observer.observe(document.body, {attributes: false, childList: true, subtree: false});
+};
